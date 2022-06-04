3432dfc8e25f8f6c67f68515846718389f649fe6
MANAN14 <56201740+MANAN14@users.noreply.github.com>
2022-06-04 17:37:28 +0530

    Frontened Updated

--- frontened/src/components/DashHeader.jsx
+++ frontened/src/components/DashHeader.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
-import '../styles/dashHeader.css'
-export  const DashHeader = (props)=>{
-    
-    return (
-        <nav className = "DashboardNav fixed-top">
-        <h3 className = "landing-name">S P L I T T E R</h3>
-     
-     <div className = "Dashfloat">
-     <NavLink to = "/login"><button className = "logoutbtn"
-    //   onClick = {()=>{
-    //    localStorage.removeItem('jwtToken');
-    //  }}
-     >Log Out</button></NavLink>
-      
-      {console.log("inside DashHeader")}
-     
-      <img className = "profile" src={require('../images/person-profile.png')} alt="" srcset=""/>
-       <label htmlFor="">{props.username}</label> 
-      
-     </div>
-        
+import "../styles/landing.css";
+export const DashHeader = (props) => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <h3 className="landing-name">S P L I T T E R</h3>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <div className="float">
+          <NavLink to="/login">
+            <button className="logoutbtn">Log Out</button>
+          </NavLink>
 
-    </nav>
-    )
-}
+          {console.log("inside DashHeader")}
 
-
-
+          <img
+            className="profile"
+            src={require("../images/person-profile.png")}
+            alt=""
+            srcset=""
+          />
+          <label htmlFor="">
+            <span style={{ color: "white" }}>{props.username}</span>
+          </label>
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
